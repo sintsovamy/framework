4fdb5b6a1e4f8ac8af456833d874796fcbe9783f
sintsovamy <msintsova18@gmail.com>
2023-11-08 13:02:24 +0700

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Src\Route;
 
-Route::add('GET', '/', [Controller\Api::class, 'index']);
 Route::add('POST', '/echo', [Controller\Api::class, 'echo']);
-
+Route::add('POST', '/login', [Controller\Api::class, 'login']);
+Route::add('POST', '/signup', [Controller\Api::class, 'signup']);
+Route::add('GET', '/posts', [Controller\Api::class, 'index']);
